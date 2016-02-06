09ea111388acbbe93e479bca49e59b55b3fd792c
Filip Matys <matysfilip@gmail.com>
2016-02-06 14:19:19 +0100

    Removed menu

--- ResultPredictor.py
+++ ResultPredictor.py
@@ -1,13 +1,7 @@
-from Menu import Menu
 from plugins.dejagnu.Parser import Parser
-
-def showHello():
-   print("Ahojky")
 
 def main():
    print("Starting...");
-   cm = Menu([("Ahoj", showHello)])
-   cm.run()
 
 # Call the main function
 if __name__ == "__main__":
