7f2af8753afcfbb33109205b05b4b713a84cb1d3
Filip Matys <matysfilip@gmail.com>
2016-04-20 15:55:49 +0200

    Added warnings into validation structure

--- utils/ValidationResult.py
+++ utils/ValidationResult.py
@@ -2,13 +2,18 @@ class ValidationResult(object):
     def __init__(self, data):
         self.data = data
         self.errors = []
+        self.warnings = []
         self.isValid = True
 
     def addError(self, message):
         self.errors.append(message)
         self.isValid = False
 
+    def addWarning(self, message):
+        self.warnings.append(message)
+
     def append(self, validation):
+        self.warnings = self.warnings + validation.warnings
         self.errors = self.errors + validation.errors
         self.isValid = self.isValid and validation.isValid
 
